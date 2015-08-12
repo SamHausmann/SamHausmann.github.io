332b42dad040ff28fefac0f7bc849d6f3d95720d
Sam Hausmann <hausmann.sam@gmail.com>
2015-08-11 23:27:17 -0400

    Adding all project files

---
+++ exp2/public/javascripts/script.js
@@ -0,0 +1,23 @@
+// tab functionality
+$(document).ready( function () {
+   $(function () {
+	$('#tabs a:first').tab('show');
+
+});
+/*
+   $.post('/', function (data){
+
+console.log('data acquired!');
+   }).done(function(){
+   	console.log('finished');
+   }).fail(function(){
+console.log('error');
+
+   });*/
+
+
+});
+
+
+
+
